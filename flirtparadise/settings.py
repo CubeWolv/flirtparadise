44b04a16aae4f9a62a443a346e2d2a6e75c56603
Imran A <74223481+CubeWolv@users.noreply.github.com>
2025-01-07 16:32:49 +0300

    added

--- flirtparadise/settings.py
+++ flirtparadise/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'gender',
     'rest_framework',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -64,9 +65,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'flirtparadise.urls'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 TEMPLATES = [
@@ -138,6 +141,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, './static/')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
