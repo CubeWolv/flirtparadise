674e5cf89f4b265ddd5665516c91cb92236e7828
Imran A <74223481+CubeWolv@users.noreply.github.com>
2025-01-10 23:25:51 +0300

    Update settings.py

--- flirtparadise/settings.py
+++ flirtparadise/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-MEDIA_URL = '/flirtparadise/media/'  # URL to serve media files
+MEDIA_URL = '/media/'  # URL to serve media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
