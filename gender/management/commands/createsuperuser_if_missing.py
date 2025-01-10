55cb0ef7ad9269cf31661f6cb77ee5446fe36f71
Imran A <74223481+CubeWolv@users.noreply.github.com>
2025-01-10 22:36:41 +0300

    super user

--- gender/management/commands/createsuperuser_if_missing.py
+++ gender/management/commands/createsuperuser_if_missing.py
@@ -1,25 +1,18 @@
-from django.apps import AppConfig
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 import os
 
-class GenderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gender'
-
-    def ready(self):
-        import gender.signals  # Import your signals here
-
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        from django.contrib.auth import get_user_model  # Import here to ensure the app registry is loaded
-        User = get_user_model()
+    help = "Create a superuser if it doesn't already exist"
 
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'password')
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(f"Superuser {username} created successfully.")
-
-
+            self.stdout.write(f"Superuser '{username}' created successfully.")
+        else:
+            self.stdout.write(f"Superuser '{username}' already exists.")
