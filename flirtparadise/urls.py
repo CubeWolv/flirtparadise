49cb1ab15951e8c144f26963854902219b238cf6
Imran A <74223481+CubeWolv@users.noreply.github.com>
2025-01-12 11:13:30 +0300

    Update urls.py

--- flirtparadise/urls.py
+++ flirtparadise/urls.py
@@ -22,9 +22,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [ 
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('', include('aichat.urls')),
     path('', include('gender.urls')),
-    path('', include('pages.urls')),
     path('', include('join.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
