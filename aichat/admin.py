5ddfc8bcdc22df6d7ea49c4243ae770e8fa12e95
Imran A <74223481+CubeWolv@users.noreply.github.com>
2025-01-19 22:04:04 +0300

    Delete aichat directory

--- aichat/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'gender', 'category', 'views')  # Display relevant fields
-    search_fields = ('name', 'category')  # Enable search by name and category
-
-# Register the Profile model with custom settings
-admin.site.register(Profile, ProfileAdmin)
